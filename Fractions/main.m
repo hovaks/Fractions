463de65bcd112964b933ad689b369843742e64e4
hovaks <hovaks@gmail.com>
2016-02-01 18:12:00 +0400

    Initial Commit

---
+++ Fractions/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fractions
+//
+//  Created by Hovak Davtyan on 01.02.16.
+//  Copyright © 2016 Hovak Davtyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
